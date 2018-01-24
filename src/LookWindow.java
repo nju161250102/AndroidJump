5995e8c9d197964b0cf9debded2f12f5b7b5fa91
161250102 <161250102@smail.nju.edu.cn>
2018-01-24 10:29:08 +0800

    增加自动，开始等控制部件

--- src/LookWindow.java
+++ src/LookWindow.java
@@ -3,28 +3,45 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
 
 public class LookWindow {
 
 	private JFrame frame = new JFrame("监视窗口");
 	private MyPanel panel = new MyPanel(); 
+	private JTextArea label = new JTextArea();
+	private JRadioButton autoButton = new JRadioButton("自动");
+	private JRadioButton midAutoButton = new JRadioButton("半自动");
 	private ScreenShot pic;
 	private MyThread task;
 	private boolean isNeedClick = false;
 	private int num = 0;
 	
 	public LookWindow() {
-		frame.setSize(1080,768);
+		frame.setSize(1280,768);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
 		
 		panel.setBounds(0, 0, 1080, 760);
-		frame.add(panel);
-		task = new MyThread();
+		JToggleButton button = new JToggleButton("开始");
+		button.setBounds(1110, 25, 130, 50);
+		label.setEditable(false);
+		JScrollPane jsp = new JScrollPane(label);
+		jsp.setBounds(1080, 150, 280, 350);
+		ButtonGroup buttonGroup = new ButtonGroup();
+		buttonGroup.add(autoButton);
+		buttonGroup.add(midAutoButton);
+		autoButton.setBounds(1110, 75, 100, 40);
+		midAutoButton.setBounds(1110, 105, 100, 40);
+		autoButton.setSelected(true);
 		
 		panel.addMouseListener(new MouseAdapter(){
 			@Override
@@ -44,9 +61,27 @@ public class LookWindow {
 			}
 		});
 		
+		button.addActionListener(e->{
+			if(!button.isSelected()) {
+				if (task != null) task.stop();
+				button.setText("开始");
+			}
+			else {
+				task = new MyThread();
+				task.start();
+				button.setText("结束");
+			}
+		});
+
+		frame.add(autoButton);
+		frame.add(midAutoButton);
+		frame.add(button);
+		frame.add(jsp);
+		frame.add(panel);
 		frame.setVisible(true);
-		task.start();
+		
 	}
+	
 	
 	class MyPanel extends JPanel {
 		public void paintComponent(Graphics g) {
@@ -63,15 +98,18 @@ public class LookWindow {
 		        try {
 		        	sleep(time);
 		        	System.out.println("序号: "+num);
+		        	addText("序号: "+num);
 					run.exec("adb shell /system/bin/screencap -p /sdcard/p.png");
 					sleep(1500);
 					run.exec("adb pull /sdcard/p.png E:\\Programs\\AndroidJump\\pic\\"+num+".png");
 					sleep(300);
 					pic = new ScreenShot("E:\\Programs\\AndroidJump\\pic\\"+num+".png");
+					addText(pic.personPointInfo());
 					panel.repaint();
 					time = (int) (pic.getLength()/737*1000);
 					pic.writeFile("E:\\Programs\\AndroidJump\\picout\\"+num+".png");
 					panel.repaint();
+					addText(pic.nextPointInfo());
 					run.exec("adb shell input swipe 500 500 501 501 "+time);
 					time += 4500;
 				} catch (IOException | InterruptedException e) {
@@ -81,6 +119,19 @@ public class LookWindow {
 					break;
 				} catch (NoNextException e) {
 					e.printStackTrace();
+					if(autoButton.isSelected()) {
+						time = (int) (pic.getLengthByAngle()/737*1000);
+						pic.writeFile("E:\\Programs\\AndroidJump\\picout\\"+num+".png");
+						panel.repaint();
+						addText(pic.nextPointInfo());
+						try {
+							run.exec("adb shell input swipe 500 500 501 501 "+time);
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						time += 4500;
+						continue;
+					}
 					JOptionPane.showMessageDialog(null, "请选择中心点");
 					isNeedClick = true;
 					num++;
@@ -89,5 +140,9 @@ public class LookWindow {
 			}
 			System.out.println("线程结束");
 		}
+	}
+	
+	private void addText(String s) {
+		label.setText(label.getText() + '\n' + s);
 	}
 }
